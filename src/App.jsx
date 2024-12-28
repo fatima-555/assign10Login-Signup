1d284951d6d87974b438d4cd4e4aa1c7ff2e5042
fatima mustufa <fatimamustufa6@gmail.com>
2024-12-27 22:38:41 -0800

    Login-SignUp page with reactJS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginSignup from './components/login-signup/loginSignup';
+// import './App.css'
+
+function App() {
+
+  return (
+     <LoginSignup />
+      
+  )
+}
+
+export default App
